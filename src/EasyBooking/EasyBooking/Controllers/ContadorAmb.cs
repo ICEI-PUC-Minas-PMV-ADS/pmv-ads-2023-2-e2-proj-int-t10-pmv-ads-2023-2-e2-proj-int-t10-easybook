903941fd972997e6460a4ce9a30d237e46639e81
Caio <128657475+caiosrz@users.noreply.github.com>
2023-11-08 09:24:32 -0300

    Add files via upload

---
+++ src/EasyBooking/EasyBooking/Controllers/ContadorAmb.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using EasyBooking.Models;
+
+namespace EasyBooking.Services 
+{
+    public class AmbienteService 
+    {
+        
+
+        public int CountAvailableAmbientes(List<AmbienteDev> ambientes)
+        {
+            return ambientes.Count(ambiente => ambiente.Disponibilidade > 0);
+        }
+    }
+}
